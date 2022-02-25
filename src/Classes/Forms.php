be329d4466a95e8ee93a20320af0b735c1954e27
Robin van Maasakker <robin@qubiqx.com>
2022-02-25 16:31:30 +0100

    Update

---
+++ src/Classes/Forms.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Qubiqx\QcommerceForms\Classes;
+
+class Forms
+{
+    public static function getPostUrl()
+    {
+        return route('qcommerce.frontend.forms.store');
+    }
+}
