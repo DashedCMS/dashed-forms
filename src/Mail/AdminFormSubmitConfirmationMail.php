0f665f2983f7e72dd6057f1a37e23b3e2df448ed
Robin van Maasakker <robin@qubiqx.com>
2022-03-30 19:18:46 +0200

    Update

---
+++ src/Mail/AdminFormSubmitConfirmationMail.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Qubiqx\QcommerceForms\Mail;
+
+use Illuminate\Support\Str;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Qubiqx\QcommerceForms\Models\Form;
+use Illuminate\Queue\SerializesModels;
+use Qubiqx\QcommerceForms\Models\FormInput;
+use Qubiqx\QcommerceCore\Models\Customsetting;
+use Qubiqx\QcommerceTranslations\Models\Translation;
+
+class AdminFormSubmitConfirmationMail extends Mailable
+{
+    use Queueable;
+    use SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(Form $form, FormInput $formInput, $replyToEmail = null)
+    {
+        $this->form = $form;
+        $this->formInput = $formInput;
+        $this->replyToEmail = $replyToEmail;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        $mail = $this->view('qcommerce-core::emails.admin-confirm-form-submit')
+            ->from(Customsetting::get('site_from_email'), Customsetting::get('company_name'))->subject(Translation::get('admin-form-confirmation-'.Str::slug($this->form->name).'-email-subject', 'forms', 'You received a new form submit!'))
+            ->with([
+                'form' => $this->form,
+                'formInput' => $this->formInput,
+            ]);
+
+        if ($this->replyToEmail) {
+            $mail->replyTo($this->replyToEmail);
+        }
+
+        return $mail;
+    }
+}
---
+++ src/Mail/FormSubmitConfirmationMail.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Qubiqx\QcommerceForms\Mail;
+
+use Illuminate\Support\Str;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Qubiqx\QcommerceForms\Models\Form;
+use Illuminate\Queue\SerializesModels;
+use Qubiqx\QcommerceForms\Models\FormInput;
+use Qubiqx\QcommerceCore\Models\Customsetting;
+use Qubiqx\QcommerceTranslations\Models\Translation;
+
+class FormSubmitConfirmationMail extends Mailable
+{
+    use Queueable;
+    use SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(Form $form, FormInput $formInput)
+    {
+        $this->form = $form;
+        $this->formInput = $formInput;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('qcommerce-core::emails.confirm-form-submit')
+            ->from(Customsetting::get('site_from_email'), Customsetting::get('company_name'))->subject(Translation::get('form-confirmation-'.Str::slug($this->form->name).'-email-subject', 'forms', 'We received your form submit!'))
+            ->with([
+                'form' => $this->form,
+                'formInput' => $this->formInput,
+            ]);
+    }
+}
