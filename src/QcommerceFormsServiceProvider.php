f13cbed650a94a32d07ce18d8e77a03d99564591
Robin van Maasakker <robin@qubiqx.com>
2022-04-03 16:15:04 +0200

    Update

--- src/QcommerceFormsServiceProvider.php
+++ src/QcommerceFormsServiceProvider.php
@@ -35,13 +35,6 @@ class QcommerceFormsServiceProvider extends PluginServiceProvider
             ->hasViews();
     }
 
-    protected function getStyles(): array
-    {
-        return array_merge(parent::getStyles(), [
-            'qcommerce-core' => str_replace('/vendor/qubiqx/qcommerce-core/src', '', str_replace('/packages/qubiqx/qcommerce-core/src', '', __DIR__)) . '/vendor/qubiqx/qcommerce-core/resources/dist/css/qcommerce-core.css',
-        ]);
-    }
-
     protected function getPages(): array
     {
         return array_merge(parent::getPages(), [
