6fa835256f660a15c742181bb0c334d84bc01f1c
Robin van Maasakker <robin@qubiqx.com>
2024-06-04 14:27:37 +0200

    Update

--- resources/views/emails/custom-confirm-form-submit.blade.php
+++ resources/views/emails/custom-confirm-form-submit.blade.php
@@ -239,9 +239,7 @@
                                                                                    style="font-family: Open Sans, Helvetica, Tahoma, Arial, sans-serif; padding: 0 10px;">
                                                                         <img alt="{{Customsetting::get('site_name')}}"
                                                                              height="auto"
-                                                                             src="{{app(\Dashed\Drift\UrlBuilder::class)->url('dashed', $logo, [
-                                                                                    'heighten' => 100
-                                                                                ])}}"
+                                                                             src="{{$logo->url ?? ''}}"
                                                                              style="border:0;display:block;outline:none;text-decoration:none;height:auto;width:100%;font-size:13px;"
                                                                              width="160"> </a></td>
                                                             @endif
