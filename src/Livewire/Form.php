5b10c86b3bfd8028d4a0748bfe8e76be9838efa0
Robin van Maasakker <robin@qubiqx.com>
2024-06-14 11:24:13 +0200

    Update

--- src/Livewire/Form.php
+++ src/Livewire/Form.php
@@ -187,7 +187,7 @@ class Form extends Component
             ->body('Je bericht is verzonden!')
             ->send();
 
-        $redirectUrl = linkHelper()->getUrl($data['redirect_after_form']);
+        $redirectUrl = linkHelper()->getUrl($this->form->redirect_after_form);
         if($redirectUrl){
             return redirect($redirectUrl);
         }
