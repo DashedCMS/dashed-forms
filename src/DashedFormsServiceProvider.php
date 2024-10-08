fb4d45dbf66b684aba06e7890052b242ed58d95b
Robin van Maasakker <robin@qubiqx.com>
2024-10-08 14:56:17 +0200

    Update

--- src/DashedFormsServiceProvider.php
+++ src/DashedFormsServiceProvider.php
@@ -31,7 +31,7 @@ class DashedFormsServiceProvider extends PackageServiceProvider
         $this->publishes([
             __DIR__ . '/../resources/templates' => resource_path('views/' . env('SITE_THEME', 'dashed')),
             __DIR__ . '/../resources/component-templates' => resource_path('views/components'),
-        ], 'dashed-forms-templates');
+        ], 'dashed-templates');
 
         cms()->builder(
             'settingPages',
