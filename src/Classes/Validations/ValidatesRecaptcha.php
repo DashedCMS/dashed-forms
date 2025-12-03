6c5af2e95ad3a1a76bed7a085c9d89df88700ee9
Robin van Maasakker <robin@dashed.nl>
2025-12-03 20:20:12 +0100

    Update

--- src/Classes/Validations/ValidatesRecaptcha.php
+++ src/Classes/Validations/ValidatesRecaptcha.php
@@ -28,6 +28,7 @@ class ValidatesRecaptcha extends LivewireAttribute
      */
     public function call(array $params, Closure $returnEarly): void
     {
+        dd($this->secretKey);
         if(!$this->secretKey){
             return;
         }
