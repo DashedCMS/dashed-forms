a5bb17cb699047737bbcb3fea1c01d5345fc0700
Robin van Maasakker <robin@dashed.nl>
2026-05-07 09:26:24 +0200

    refactor(forms): use captcha components in package form template

--- resources/templates/forms/form.blade.php
+++ resources/templates/forms/form.blade.php
@@ -1,4 +1,4 @@
-<form class="space-y-8 p-4 bg-primary-800 text-white rounded-lg" wire:submit="submit" @if(Customsetting::get('google_recaptcha_site_key')) wire:recaptcha @endif>
+<form class="space-y-8 p-4 bg-primary-800 text-white rounded-lg" wire:submit="submit" @captchaFormAttributes>
 {{--    @if($blockData['title'] ?? false)--}}
 {{--        <h2 class="text-2xl font-bold tracking-tight md:text-3xl font-headline">{{ $blockData['title'] }}</h2>--}}
 {{--    @endif--}}
@@ -23,9 +23,7 @@
         </div>
     @endif
 
-    @if($errors->has('gRecaptchaResponse'))
-        <div class="alert alert-danger">{{ $errors->first('gRecaptchaResponse') }}</div>
-    @endif
+    <x-dashed-forms::captcha-errors />
 
     <div class="grid gap-4 md:grid-cols-2">
         @foreach($this->formFields as $field)
@@ -66,11 +64,5 @@
             @if($buttonTitle)
         </div>
     @endif
-    @if(Customsetting::get('google_recaptcha_site_key'))
-        @livewireRecaptcha(
-        version: 'v3',
-        siteKey: Customsetting::get('google_recaptcha_site_key'),
-        size: 'compact',
-        )
-    @endif
+    <x-dashed-forms::captcha />
 </form>
